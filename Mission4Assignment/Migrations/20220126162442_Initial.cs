427fa83c43d050c41c05839f9ebe4bdb8ed6b6c1
delangea <73494880+delangea@users.noreply.github.com>
2022-02-01 09:36:35 -0700

    initial commit

---
+++ Mission4Assignment/Migrations/20220126162442_Initial.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mission4Assignment.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "responses",
+                columns: table => new
+                {
+                    MovieID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(nullable: false),
+                    Category = table.Column<string>(nullable: false),
+                    Year = table.Column<int>(nullable: false),
+                    Director = table.Column<string>(nullable: false),
+                    Rating = table.Column<string>(nullable: false),
+                    Edited = table.Column<bool>(nullable: false),
+                    LentTo = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(maxLength: 25, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_responses", x => x.MovieID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "MovieID", "Category", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 1, "Comedy", "Jason Moore", false, null, null, "PG-13", "Pitch Perfect", 2012 });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "MovieID", "Category", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 2, "Period Drama", "Greta Gerwig", false, null, null, "PG", "Little Women", 2019 });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "MovieID", "Category", "Director", "Edited", "LentTo", "Notes", "Rating", "Title", "Year" },
+                values: new object[] { 3, "Horror", "Wes Craven", false, null, null, "R", "Scream", 1996 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "responses");
+        }
+    }
+}
---
+++ Mission4Assignment/Models/MovieContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4Assignment.Models
+{
+    public class MovieContext : DbContext
+    {
+        //Constructor
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options)
+        {
+            //Leave blank for now
+        }
+
+        public DbSet<Movie> responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Movie>().HasData(
+                new Movie
+                {
+                    MovieID=1,
+                    Title = "Pitch Perfect",
+                    Year = 2012,
+                    Category="Comedy",
+                    Director="Jason Moore",
+                    Rating="PG-13"
+                },
+                new Movie
+                {
+                    MovieID = 2,
+                    Title = "Little Women",
+                    Year = 2019,
+                    Category="Period Drama",
+                    Director="Greta Gerwig",
+                    Rating="PG"
+                },
+                new Movie
+                {
+                    MovieID = 3,
+                    Title = "Scream",
+                    Year = 1996,
+                    Category = "Horror",
+                    Director = "Wes Craven",
+                    Rating = "R"
+                }
+            );
+        }
+    }
+}
